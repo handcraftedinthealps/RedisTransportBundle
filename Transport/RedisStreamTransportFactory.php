b8c408bc046aa2689429eac783856bfa3d667b11
Alexander Schranz <alexander@sulu.io>
2018-12-12 17:32:09 +0100

    make dsn parsing more readable

--- Transport/RedisStreamTransportFactory.php
+++ Transport/RedisStreamTransportFactory.php
@@ -28,7 +28,11 @@ class RedisStreamTransportFactory implements TransportFactoryInterface
             );
         }
 
-        list(, $stream, $group, $consumer) = array_merge(explode('/', $parsedUrl['path']), [null, null]);
+        $dsnParts = explode('/', $parsedUrl['path']);
+
+        $stream = $dsnParts[1];
+        $group = $dsnParts[2] ?? null;
+        $consumer = $dsnParts[3] ?? null;
 
         return new RedisStreamTransport($parsedUrl['host'], $parsedUrl['port'], $stream, $group, $consumer);
     }
